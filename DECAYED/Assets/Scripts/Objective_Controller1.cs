d6b77c9db00f1ed628e930425ea2b5f8e1efb936
MAD_DOGGO <tkatlrdldi@gmail.com>
2023-11-21 01:29:10 +0900

    Remove some and edit some

---
+++ DECAYED/Assets/Scripts/Objective_Controller1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Objective_Controller1 : MonoBehaviour
+{
+    public TextMeshProUGUI P_Text;
+    public TextMeshProUGUI G_Text;
+    public TextMeshProUGUI Obj_Text;
+
+    public Player_Move PM;
+
+    public string G_String;
+    public string P_String;
+    public string Obj_String;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PM = FindObjectOfType<Player_Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        CancelInvoke();
+        PM.Invoke("G_ResetText", 8f);
+        PM.Invoke("P_ResetText", 8f);
+        Set_G(G_String);
+        Set_P(P_String);
+        Set_Obj(Obj_String);
+    }
+
+    public void Set_G(string text)
+    {
+        G_Text.text = "∏Ò«• :\n" + text;
+    }
+
+    public void Set_P(string text)
+    {
+        P_Text.text = text;
+    }
+
+    public void Set_Obj(string text)
+    {
+        Obj_Text.text = text;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            return;
+        }
+    }
+}
